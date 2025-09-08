51ba074d5b61ea77acd106c6fc0fe521bd106c82
Ritik Singh <rsbaghel9314@gmail.com>
2025-09-08 23:56:36 +0530

    Update axiosInstance.js

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000",
+    baseURL:"https://url5-3.onrender.com",
     timeout:10000, //10s
     withCredentials:true
 })
